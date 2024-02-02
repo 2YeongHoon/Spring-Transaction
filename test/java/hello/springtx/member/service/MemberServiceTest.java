7df7ad640ff5a4e502ce93fdc00a9d2f5084e825
2Yeonghoon <djaakclsrn12@naver.com>
2024-02-02 23:14:43 +0900

    TST: 서비스 계층에 트랜잭션이 없는경우 테스트 추가

--- test/java/hello/springtx/member/service/MemberServiceTest.java
+++ test/java/hello/springtx/member/service/MemberServiceTest.java
@@ -1,6 +1,7 @@
 package hello.springtx.member.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import hello.springtx.member.repository.LogRepository;
 import hello.springtx.member.repository.MemberRepository;
@@ -36,6 +37,26 @@ class MemberServiceTest {
     //then: 모든 데이터가 정상 저장된다.
     assertTrue(memberRepository.find(username).isPresent());
     assertTrue(logRepository.find(username).isPresent());
+  }
+
+  /**
+   * MemberService @Transactional:OFF
+   * MemberRepository @Transactional:ON
+   * LogRepository @Transactional:ON Exception
+   */
+  @Test
+  void outerTxOff_fail() {
+
+    //given
+    String username = "로그예외_outerTxOff_fail";
+
+    //when
+    assertThatThrownBy(() -> memberService.joinV1(username))
+        .isInstanceOf(RuntimeException.class);
+
+    //then: 완전히 롤백되지 않고, member 데이터가 남아서 저장된다.
+    assertTrue(memberRepository.find(username).isPresent());
+    assertTrue(logRepository.find(username).isEmpty());
   }
 
 }
