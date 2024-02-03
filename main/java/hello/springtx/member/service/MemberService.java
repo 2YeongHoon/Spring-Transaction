6c3595aea8e834a0a72215f9f522923d109b2877
2Yeonghoon <djaakclsrn12@naver.com>
2024-02-03 23:49:07 +0900

    TST: 서비스 로직에 트랜잭션을 추가하여 물리 트랜잭션으로 묶어주기

--- main/java/hello/springtx/member/service/MemberService.java
+++ main/java/hello/springtx/member/service/MemberService.java
@@ -7,6 +7,7 @@ import hello.springtx.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -16,6 +17,7 @@ public class MemberService {
   private final MemberRepository memberRepository;
   private final LogRepository logRepository;
 
+  @Transactional
   public void joinV1(String username) {
     Member member = new Member(username);
     Log logMessage = new Log(username);
