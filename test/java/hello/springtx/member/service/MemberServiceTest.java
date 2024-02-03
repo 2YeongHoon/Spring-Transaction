654729c190823bdb69c9cd52a550eb24d930cd29
2Yeonghoon <djaakclsrn12@naver.com>
2024-02-03 23:54:11 +0900

    TST: 모든 메서드에 트랜잭션을 추가하여 물리 트랜잭션으로 묶어주기

--- test/java/hello/springtx/member/service/MemberServiceTest.java
+++ test/java/hello/springtx/member/service/MemberServiceTest.java
@@ -22,8 +22,9 @@ class MemberServiceTest {
   LogRepository logRepository;
 
   /**
-   * MemberService @Transactional:OFF MemberRepository @Transactional:ON LogRepository
-   * @Transactional:ON
+   * MemberService @Transactional:OFF
+   * MemberRepository @Transactional:ON
+   * LogRepository @Transactional:ON
    */
   @Test
   void outerTxOff_success() {
@@ -69,6 +70,25 @@ class MemberServiceTest {
 
     //given
     String username = "singleTx";
+
+    //when
+    memberService.joinV1(username);
+
+    //then: 모든 데이터가 정상 저장된다.
+    assertTrue(memberRepository.find(username).isPresent());
+    assertTrue(logRepository.find(username).isPresent());
+  }
+
+  /**
+   * MemberService @Transactional:ON
+   * MemberRepository @Transactional:ON
+   * LogRepository @Transactional:ON
+   */
+  @Test
+  void outerTxOn_success() {
+
+    //given
+    String username = "outerTxOn_success";
 
     //when
     memberService.joinV1(username);
